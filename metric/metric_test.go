220a8d649d5f647ed76116364b50681000b8974b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-14 14:32:32 +0800

    metric_test.go: update TestBOOL_MarshalText and TestBOOL_UnmarshalText.

--- metric/metric_test.go
+++ metric/metric_test.go
@@ -8,14 +8,12 @@ import (
 
 func TestBOOL_MarshalText(t *testing.T) {
 	t.Run("true", func(t *testing.T) {
-		TRUE := BOOL(1)
 		output, err := TRUE.MarshalText()
 		require.NoError(t, err)
 		require.Equal(t, output, []byte("true"))
 	})
 
 	t.Run("false", func(t *testing.T) {
-		FALSE := BOOL(0)
 		output, err := FALSE.MarshalText()
 		require.NoError(t, err)
 		require.Equal(t, output, []byte("false"))
@@ -28,11 +26,11 @@ func TestBOOL_UnmarshalText(t *testing.T) {
 
 		err := b.UnmarshalText([]byte("true"))
 		require.NoError(t, err)
-		require.Equal(t, BOOL(1), b)
+		require.Equal(t, TRUE, b)
 
 		err = b.UnmarshalText([]byte("True"))
 		require.NoError(t, err)
-		require.Equal(t, BOOL(1), b)
+		require.Equal(t, TRUE, b)
 	})
 
 	t.Run("false", func(t *testing.T) {
@@ -40,11 +38,11 @@ func TestBOOL_UnmarshalText(t *testing.T) {
 
 		err := b.UnmarshalText([]byte("false"))
 		require.NoError(t, err)
-		require.Equal(t, BOOL(0), b)
+		require.Equal(t, FALSE, b)
 
 		err = b.UnmarshalText([]byte("False"))
 		require.NoError(t, err)
-		require.Equal(t, BOOL(0), b)
+		require.Equal(t, FALSE, b)
 	})
 
 	t.Run("invalid", func(t *testing.T) {

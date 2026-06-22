ab9fe3771683a77d9c89e6a0b107cda1dbad80ff
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-06-22 12:43:48 +0800

    info.go: replace the hash field to Hash [32]byte, remove structure tag.

--- info/info.go
+++ info/info.go
@@ -2,8 +2,8 @@ package info
 
 // Info contains the raw runtime information.
 type Info struct {
-	Version uint64 `json:"version"`
-	Hash    uint64 `json:"hash"`
-	Size    uint32 `json:"size"`
-	Flags   uint32 `json:"flags"`
+	Version uint64
+	Hash    [32]byte
+	Size    uint32
+	Flags   uint32
 }
